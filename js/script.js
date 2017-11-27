c50ef005fcc880b57424955ec4e0e0bdaa309731
m.nowosielski <michal.nowosielski.90@gmail.com>
2017-11-27 17:19:24 +0100

    finall

--- js/script.js
+++ js/script.js
@@ -1,21 +1,60 @@
+var $imgClass = $('.imgClass');
+var $description = $('.description');
+var $wikiElem = $('#wikipedia-links');
+var $nytHeaderElem = $('#nytimes-header');
+var $nytElem = $('#nytimes-articles');
+var $greeting = $('#greeting');
 
 function loadData() {
 
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
+   // clear out old data before new request
+   $wikiElem.text("");
+   $nytElem.text("");
 
-    // clear out old data before new request
-    $wikiElem.text("");
-    $nytElem.text("");
-
-    // load streetview
-
-    // YOUR CODE GOES HERE!
-
-    return false;
+   return false;
 };
+
+$('#submit-btn').on('click', function () {
+
+   var street = $('#street').val();
+   var city = $('#city').val();
+   var address = street + ',' + city;
+   var response;
+   var data;
+  
+   //Google maps
+   $imgClass.append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address + '"/>');
+ 
+   //Wikipedia
+   $.ajax({
+      url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&titles=' + city,
+      dataType: "jsonp",
+      jsonp: "callback",
+      success: function (data) {
+
+         var obj = data.query.pages;
+         var content = obj[Object.keys(obj)[0]];
+         $description.append(content.extract);
+      }
+   });
+   
+//New York Times
+   var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + city + '&sort=newest&api-key=69ca3450a93f421ea0d1df6402c49630';
+   $.getJSON(nytimesUrl, function (data) {
+
+      $nytHeaderElem.text('New York Times Articles About ' + city);
+      articles = data.response.docs;
+      for (var i = 0; i < articles.length; i++) {
+         var article = articles[i];
+         $nytElem.append('<li class="article">' +
+                 '<a href="' + article.web_url + '">' + article.headline.main + '</a>' +
+                 '<p>' + article.snippet + '</p>' +
+                 '</li>');
+      };
+   })
+           .error(function (e) {
+              $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+           });
+});
 
 $('#form-container').submit(loadData);
